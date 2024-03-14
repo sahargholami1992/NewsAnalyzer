c812eaeaf3fe9b5e09e2d76342e17f8e500ffafe
sahargholami1992 <sahar_gholami03@gmail.com>
2024-03-14 18:57:19 +0330

    edit NewsItem

--- src/main/java/org/example/entity/NewsItem.java
+++ src/main/java/org/example/entity/NewsItem.java
@@ -2,33 +2,17 @@ package org.example.entity;
 
 
 
+import lombok.*;
+
 import java.io.Serializable;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class NewsItem implements Serializable {
-    private static final long serialVersionUID = 12345L;
     private String headline;
     private int priority;
 
-
-    public NewsItem(String headline, int priority) {
-        this.headline = headline;
-        this.priority = priority;
-    }
-
-    public String getHeadline() {
-        return headline;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-
-    @Override
-    public String toString() {
-        return "NewsItem{" +
-                "headline='" + headline + '\'' +
-                ", priority=" + priority +
-                '}';
-    }
 }
