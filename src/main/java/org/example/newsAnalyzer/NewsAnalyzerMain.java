4d9f50ac0ae37f01197cd9ffb9c6d833263a388a
sahargholami1992 <sahar_gholami03@gmail.com>
2024-03-14 18:55:13 +0330

    commit main

---
+++ src/main/java/org/example/newsAnalyzer/NewsAnalyzerMain.java
@@ -0,0 +1,17 @@
+package org.example.newsAnalyzer;
+
+
+import org.example.util.ClientHandler;
+import org.example.util.ReportTask;
+
+import java.util.Timer;
+
+
+public class NewsAnalyzerMain {
+    public static void main(String[] args) {
+        Thread clientThread = new Thread(new ClientHandler());
+        clientThread.start();
+        Timer timer = new Timer();
+        timer.schedule(new ReportTask(), 0, 10000);
+    }
+}
